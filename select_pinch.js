ff188ea10af61f7a3993fa96a05dfa572382ea3b
jooam <ja960508@naver.com>
2021-07-29 17:21:21 +0900

    set pinch limiter

--- select_pinch.js
+++ select_pinch.js
@@ -2,12 +2,14 @@
 const evCache = new Array();
 let prevDiff = -1;
 let pinchValidCheck = true;
-const output = document.querySelector('.output');
+// 핀치 간격을 설정
+const pinchLimit = 15;
+// const output = document.querySelector('.output');
 
 function pointerdown_handler(ev) {
   //    pointerdown 이벤트는 터치 반응이 시작을 알려준다.
   //    두 손가락이 터치하는 이벤트를 감지하기 위해 caching
-  output.innerHTML += `down <br/>`;
+  // output.innerHTML += `down <br/>`;
   evCache.push(ev);
 }
 
@@ -32,42 +34,43 @@ function pointermove_handler(ev) {
     const curDiffY = Math.floor(
       Math.abs(evCache[0].clientY - evCache[1].clientY)
     );
-
     const curDiff = Math.floor(
       Math.sqrt(Math.pow(curDiffX, 2) + Math.pow(curDiffY, 2))
     );
 
+    // 가장 처음 터치 위치를 기록하기 위해 validation 진행
     pinchValidCheck ? (prevDiff = curDiff) : (prevDiff = prevDiff);
     pinchValidCheck = false;
 
-    if (curDiff > prevDiff && curDiff - prevDiff > 10) {
+    if (curDiff > prevDiff && curDiff - prevDiff > pinchLimit) {
       // Zoom in(두 점이 멀어질 때) 발생하는 이벤트
       !voteList.classList.contains('zoom') && voteList.classList.add('zoom');
     }
-    if (curDiff < prevDiff && prevDiff - curDiff > 10) {
+    if (curDiff < prevDiff && prevDiff - curDiff > pinchLimit) {
       // Zoom out시 발생하는 이벤트
       voteList.classList.contains('zoom') && voteList.classList.remove('zoom');
     }
-    output.innerHTML += `curDiff: ${curDiff} <br/>curDiffX: ${curDiffX} <br/>  curDiffY: ${curDiffY} <br/> prevDiff: ${prevDiff} <br/>`;
+    // output.innerHTML += `curDiff: ${curDiff} <br/>curDiffX: ${curDiffX} <br/>  curDiffY: ${curDiffY} <br/> prevDiff: ${prevDiff} <br/>`;
 
     // Cache the distance for the next move event
   }
 }
 
-function dt() {
-  const output = document.querySelector('.output');
-  output.innerHTML = ``;
-  output.style.display === 'none'
-    ? (output.style.display = 'block')
-    : (output.style.display = 'none');
-}
+// 디버깅용
+// function dt() {
+//   const output = document.querySelector('.output');
+//   output.innerHTML = ``;
+//   output.style.display === 'none'
+//     ? (output.style.display = 'block')
+//     : (output.style.display = 'none');
+// }
 
 // 손가락을 땔 때 시행하는 이벤트 handler
 function pointerup_handler(ev) {
   //   이벤트를 제거하여 cache를 초기화시킨다.
   remove_event(ev);
   pinchValidCheck = true;
-  output.innerHTML += `point up check: ${pinchValidCheck}`;
+  // output.innerHTML += `point up check: ${pinchValidCheck}`;
   // If the number of pointers down is less than two then reset diff tracker
   if (evCache.length < 2) prevDiff = -1;
 }
